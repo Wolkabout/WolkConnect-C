980bed7674556717b1b7d5faaa57516f487a43d0
Sava Vranesevic <sava.vranesevic@wolkabout.com>
2018-04-16 11:07:00 +0200

    Assert that given in memory persistence is large enough to hold at least one item

--- sources/in_memory_persistence.c
+++ sources/in_memory_persistence.c
@@ -18,6 +18,7 @@
 #include "circular_buffer.h"
 #include "outbound_message.h"
 #include "persistence.h"
+#include "wolk_utils.h"
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -27,6 +28,8 @@ static circular_buffer_t buffer;
 void in_memory_persistence_init(void* storage, uint32_t size, bool wrap)
 {
     uint32_t num_elements = size / sizeof(outbound_message_t);
+    WOLK_ASSERT(num_elements > 0);
+    
     circular_buffer_init(&buffer, storage, num_elements, sizeof(outbound_message_t), wrap, true);
 }
 
