ad48c50c6fce78ad623ee549a5feaa2d7614bf79
srdjanStankovic <srdjanrstankovic@gmail.com>
2020-01-10 15:52:38 +0100

    generate new doc

--- doc/html/search/pages_0.js
+++ doc/html/search/pages_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['readme',['README',['../md_examples_full_feature_set_README.html',1,'']]],
-  ['readme',['README',['../md_README.html',1,'']]]
+  ['readme',['README',['../md_examples_full_feature_set__r_e_a_d_m_e.html',1,'']]],
+  ['readme',['README',['../md__r_e_a_d_m_e.html',1,'']]]
 ];
