c5f1f8854589d24fcca15178e84ae60e34046d3d
srdjastankovic <srdjan.stankovic@wolkabout.com>
2020-06-09 11:40:59 +0200

    add more test

--- tests/test/test_reading.c
+++ tests/test/test_reading.c
@@ -15,7 +15,7 @@ void tearDown(void)
 {
 }
 
-void test_readings_init(void)
+void test_reading_readings_init(void)
 {
     manifest_item_t string_sensor;
     manifest_item_init(&string_sensor, "T", READING_TYPE_SENSOR, DATA_TYPE_STRING);
@@ -29,6 +29,68 @@ void test_readings_init(void)
     TEST_ASSERT_EQUAL_STRING("T", reading.manifest_item.reference);
 
     reading_clear(&reading);
+    TEST_ASSERT_EQUAL_STRING("", reading.reading_data);
+}
+
+void test_reading_readings_clear()
+{
+    manifest_item_t string_sensor;
+    manifest_item_init(&string_sensor, "reference", READING_TYPE_SENSOR, DATA_TYPE_STRING);
+
+    reading_init(&reading, &string_sensor);
+    reading_set_data(&reading, "SOME TEST READINGS");
+    TEST_ASSERT_EQUAL_STRING("SOME TEST READINGS", reading.reading_data);
+
+    reading_clear(&reading);
+
+    TEST_ASSERT_EQUAL_INT(ACTUATOR_STATE_READY, reading.actuator_status);
+    TEST_ASSERT_EQUAL_INT(0, reading.rtc);
+    TEST_ASSERT_EQUAL_STRING("", reading.reading_data);
+}
+
+void test_reading_reading_set_data(void)
+{
+    manifest_item_t string_sensor;
+    manifest_item_init(&string_sensor, "reference", READING_TYPE_SENSOR, DATA_TYPE_STRING);
+
+    reading_init(&reading, &string_sensor);
+    reading_set_data(&reading, "SOME TEST READINGS");
+
+    TEST_ASSERT_EQUAL_STRING("SOME TEST READINGS", reading.reading_data);
+
+    reading_clear(&reading);
+    TEST_ASSERT_EQUAL_STRING("", reading.reading_data);
+}
+
+void test_reading_reading_get_delimited_data(void)
+{
+    char data_buffer[PARSER_INTERNAL_BUFFER_SIZE];
+    int8_t values_size = 2;
+    manifest_item_t string_sensor;
+    manifest_item_init(&string_sensor, "reference", READING_TYPE_SENSOR, DATA_TYPE_STRING);
+    manifest_item_set_reading_dimensions_and_delimiter(&string_sensor, values_size, DATA_DELIMITER);
+
+    reading_init(&reading, &string_sensor);
+    reading_set_rtc(&reading, 1591621716);
+
+    for (uint32_t i = 0; i < values_size; ++i) {
+        reading_set_data_at(&reading, "SOME TEST READINGS", i);
+    }
+
+    TEST_ASSERT(reading_get_delimited_data(&reading, data_buffer, PARSER_INTERNAL_BUFFER_SIZE));
+
+    reading_clear(&reading);
+    TEST_ASSERT_EQUAL_STRING("", reading.reading_data);
+}
+
+void test_reading_reading_set_data_at(void)
+{
+    reading_set_data_at(&reading, "SOME TEST READING", 0);
+
+    TEST_ASSERT_EQUAL_STRING("SOME TEST READING", reading.reading_data);
+
+    reading_clear(&reading);
+    TEST_ASSERT_EQUAL_STRING("", reading.reading_data);
 }
 
 void test_reading_set_rtc(void)
