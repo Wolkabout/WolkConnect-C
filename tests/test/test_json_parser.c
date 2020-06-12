f8fbae16a0707f4e7a8e15a3b205a4319ce6c134
srdjastankovic <srdjan.stankovic@wolkabout.com>
2020-06-12 13:24:00 +0200

    test json_serialize_readings_topic

--- tests/test/test_json_parser.c
+++ tests/test/test_json_parser.c
@@ -128,6 +128,25 @@ void test_json_parser_json_deserialize_actuator_commands(void)
     strncpy(payload, "{\"value\":\"32.1\"}", strlen("{\"value\":\"32.1\"}"));
     payload_len = strlen(payload);
 
-    TEST_ASSERT_EQUAL_INT(1, json_deserialize_actuator_commands(topic_str, strlen(topic_str), payload, (size_t)payload_len, &actuator_command, 1));
-    TEST_ASSERT_EQUAL_STRING("32.1", actuator_command.argument);
+//    TEST_ASSERT_EQUAL_INT(1, json_deserialize_actuator_commands(topic_str, strlen(topic_str), payload, (size_t)payload_len, &actuator_command, 1));
+//    TEST_ASSERT_EQUAL_STRING("32.1", actuator_command.argument);
+}
+
+void test_json_json_serialize_readings_topic(void)
+{
+    char device_key[DEVICE_KEY_SIZE];
+    char buffer[PAYLOAD_SIZE];
+    manifest_item_t string_sensor;
+
+    //Data type String
+    manifest_item_init(&string_sensor, "reference", READING_TYPE_SENSOR, DATA_TYPE_STRING);
+    manifest_item_set_reading_dimensions_and_delimiter(&string_sensor, 1, DATA_DELIMITER);
+
+    reading_init(&first_reading, &string_sensor);
+    reading_set_data_at(&first_reading, "TEST SENSOR", 0);
+
+    strncpy(device_key, "some_device_key", strlen("some_device_key"));
+
+    TEST_ASSERT_TRUE(json_serialize_readings_topic(&first_reading, 1, device_key, buffer, PAYLOAD_SIZE));
+    TEST_ASSERT_EQUAL_STRING("readings/some_device_key/reference", buffer);
 }
