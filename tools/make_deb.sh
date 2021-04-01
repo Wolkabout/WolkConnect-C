fb613bf993395884d1a5c3c19d9a5bad76390bd2
srdjastankovic <srdjan.stankovic@wolkabout.com>
2021-04-01 20:52:58 +0200

    add more excludes;add append to file in logs

--- tools/make_deb.sh
+++ tools/make_deb.sh
@@ -1,13 +1,12 @@
 echo "Starting to build .deb"
 
 #Pack existing source and move to tools dir
-tar -zcvf wolkconnect-c_0.0.orig.tar.gz --exclude=.git ../../WolkConnect-C
+tar -zcvf wolkconnect-c_0.0.orig.tar.gz --exclude=.git --exclude=package --exclude=obj --exclude=out/bin \
+--exclude=out/CMake* --exclude=out/cmake_install.cmake --exclude=out/Makefile ../../WolkConnect-C
 tar -zxvf wolkconnect-c_0.0.orig.tar.gz
 
 #make .deb
-cd WolkConnect-C
-debuild -us -uc -b -j$(nproc)
+cd WolkConnect-C && debuild -us -uc -b -j$(nproc)
 
 #Remove unnecessary
-cd ../
-rm -r WolkConnect-C && rm *.build *.buildinfo *.changes *.tar.gz
+cd ../ && rm -r WolkConnect-C && rm *.ddeb *.build *.buildinfo *.changes *.tar.gz
