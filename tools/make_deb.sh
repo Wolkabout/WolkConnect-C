3e69a29fae01b865ce345ed056f16a97813da5d3
srdjastankovic <srdjan.stankovic@wolkabout.com>
2021-04-02 16:54:51 +0200

    exclude build dir

--- tools/make_deb.sh
+++ tools/make_deb.sh
@@ -1,7 +1,7 @@
 echo "Starting to build .deb"
 
 #Pack existing source and move to tools dir
-tar -zcvf wolkconnect-c_0.0.orig.tar.gz --exclude=.git --exclude=package --exclude=obj --exclude=out/bin \
+tar -zcvf wolkconnect-c_0.0.orig.tar.gz --exclude=build --exclude==tools --exclude=.git --exclude=out/bin \
 --exclude=out/CMake* --exclude=out/cmake_install.cmake --exclude=out/Makefile ../../WolkConnect-C
 tar -zxvf wolkconnect-c_0.0.orig.tar.gz
 
