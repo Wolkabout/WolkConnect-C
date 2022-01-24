b73ae627b7e0040b9ce1421ba63fbd84f88362a7
Lazar Bozic <lazar.bozic@wolkabout.com>
2022-01-24 13:37:01 +0100

    Feed model

--- sources/model/feed.c
+++ sources/model/feed.c
@@ -1,3 +1,10 @@
-//
-// Created by lazar on 15.1.22..
-//
+#include "feed.h"
+#include <string.h>
+
+void initialize_feed(feed_t* feed, char* name, const char* reference, char* unit, const feed_type_t feedType)
+{
+    strncpy(feed->name, name, MANIFEST_ITEM_NAME_SIZE);
+    strncpy(feed->reference, reference, MANIFEST_ITEM_REFERENCE_SIZE);
+    strncpy(feed->unit, unit, MANIFEST_ITEM_UNIT_SIZE);
+    feed->feedType = feedType;
+}
--- sources/model/feed.h
+++ sources/model/feed.h
@@ -1,8 +1,25 @@
-//
-// Created by lazarb on 14.1.22..
-//
+#ifndef FEED_H
+#define FEED_H
 
-#ifndef WOLKCONNECTOR_C_FEED_H
-#define WOLKCONNECTOR_C_FEED_H
+#include "size_definitions.h"
+#include "types.h"
 
-#endif // WOLKCONNECTOR_C_FEED_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+    char name[MANIFEST_ITEM_NAME_SIZE];
+    char reference[MANIFEST_ITEM_REFERENCE_SIZE];
+    char unit[MANIFEST_ITEM_UNIT_SIZE];
+
+    feed_type_t feedType;
+} feed_t;
+
+void initialize_feed(feed_t* feed, char* name, const char* reference, char* unit, feed_type_t feedType);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // FEED_H
